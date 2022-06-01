11161323c5377ae0eb373b06808e6bfde3a1d83a
Hector <hevtot@gmail.com>
2022-05-31 21:56:15 -0600

    Server hector

---
+++ serverHector.py
@@ -0,0 +1,19 @@
+from contextlib import redirect_stderr
+from unittest import result
+from flask import Flask, redirect, url_for, jsonify
+from flask import render_template
+from flask import Response
+from collections import Counter
+from email.mime import image
+from functools import total_ordering
+import pickle
+from types import coroutine
+
+app = Flask(__name__)
+
+@app.route("/")
+def test():
+    return "Hola"
+
+if __name__ == "__main__":
+     app.run(debug=True)
